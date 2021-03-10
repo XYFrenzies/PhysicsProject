0a1bad86f52be061d50af70f8ffdf083de17167d
Benjamin McDonald <s172834@students.aie.edu.au>
2021-03-10 12:34:07 +1100

    I have added a mesh class for rendeing graphics and shaders.

--- aieBootstrap-master/aieBootstrap-master/GraphicsProject/Mesh.h
+++ aieBootstrap-master/aieBootstrap-master/GraphicsProject/Mesh.h
@@ -1,12 +1,26 @@
 #pragma once
+#include <glm/glm.hpp>
 class Mesh
 {
 public:
-	Mesh();
+	Mesh() : triCount(0), vao(0), vbo(0), ibo(0) {};
 	virtual ~Mesh();
+
+	struct Vertex 
+	{
+		glm::vec4 position;
+		glm::vec4 normal;
+		glm::vec2 textCoord;
+	};
+	void InitialiseQuad();
+	virtual void Draw();
+
 private:
 
 protected:
-
+	unsigned int	triCount;
+	unsigned int	vao, //Vertex Array Object
+					vbo, //Vectex Buffer Object
+					ibo; //Index Buffer Object
 };
 

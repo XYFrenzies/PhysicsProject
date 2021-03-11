365065e39e6cb8e3998818a8e532c33baabf3270
Benjamin McDonald <s172834@students.aie.edu.au>
2021-03-11 14:55:03 +1100

    I have gotten the rotation of the objects and have added 4 different objects into the scene.

--- aieBootstrap-master/aieBootstrap-master/GraphicsProject/GraphicsProjectApp.cpp
+++ aieBootstrap-master/aieBootstrap-master/GraphicsProject/GraphicsProjectApp.cpp
@@ -10,7 +10,9 @@ using glm::vec4;
 using glm::mat4;
 using aie::Gizmos;
 
-GraphicsProjectApp::GraphicsProjectApp() {
+GraphicsProjectApp::GraphicsProjectApp(): m_ambientLight(), m_bunnyTransform(), m_dragonTransform(), 
+m_light(), m_planet(), m_projectionMatrix(), m_viewMatrix() 
+{
 
 }
 
@@ -30,6 +32,8 @@ bool GraphicsProjectApp::startup() {
 	m_projectionMatrix = glm::perspective(glm::pi<float>() * 0.25f, 
 		(float)getWindowWidth() / (float)getWindowHeight(), 0.1f, 1000.0f);
 
+	m_light.colour = { 1, 0, 1 };
+	m_ambientLight = { 0.1f, 1, 1 };
 	return LoadShaderAndMesh();
 }
 
@@ -67,7 +71,27 @@ void GraphicsProjectApp::update(float deltaTime) {
 
 	// add a transform so that we can see the axis
 	Gizmos::addTransform(mat4(1));
-	m_quadTransform = glm::rotate(m_quadTransform, atan(1.14f) * deltaTime, glm::vec3(10, 0, -10));
+
+	IMGUI_Logic();
+	IMGUI_Transform();
+	//Rotates the light direction per time variable.
+	float time = getTime();
+	m_light.direction = glm::normalize(glm::vec3(glm::cos(time * 2), glm::sin(time * 2), 0));
+	//Rotates the plane per update.
+	
+	//BunnyRotation
+	m_bunnyRot = glm::rotate(m_bunnyRot, deltaTime, glm::vec3(0, 1, 0));
+	m_bunnyTransform = m_bunnyTranslation * m_bunnyRot * m_bunnyScale;
+	//DragonRotation
+	m_dragonRot = glm::rotate(m_dragonRot, deltaTime, glm::vec3(0, 1, 0));
+	m_dragonTransform = m_dragonTranslation * m_dragonRot * m_dragonScale;
+	//BuddhaRotation
+	m_buddhaRot = glm::rotate(m_buddhaRot, deltaTime, glm::vec3(0, 1, 0));
+	m_buddhaTransform = m_buddhaTranslation * m_buddhaRot * m_buddhaScale;
+	//LucyRotation
+	m_lucyRot = glm::rotate(m_lucyRot, deltaTime, glm::vec3(0, 1, 0));
+	m_lucyTransform = m_lucyTranslation * m_lucyRot * m_lucyScale;
+
 	// quit if we press escape
 	aie::Input* input = aie::Input::getInstance();
 
@@ -98,7 +122,9 @@ void GraphicsProjectApp::Planets()
 
 bool GraphicsProjectApp::LoadShaderAndMesh()
 {
-#pragma region Quad
+#pragma region Load Shaders
+#pragma region QuadShader
+
 	//Load the vertex shader from a file
 	m_simpleShader.loadShader(aie::eShaderStage::VERTEX, "./shaders/simple.vert");
 
@@ -109,6 +135,57 @@ bool GraphicsProjectApp::LoadShaderAndMesh()
 		printf("Simple Shader had an error: %s\n", m_simpleShader.getLastError());
 		return false;
 	}
+
+#pragma endregion 
+#pragma region BunnyShader
+	m_bunnyShader.loadShader(aie::eShaderStage::VERTEX, "./shaders/simple.vert");
+
+	//Load the fragment shader from the file
+	m_bunnyShader.loadShader(aie::eShaderStage::FRAGMENT, "./shaders/simple.frag");
+	if (!m_bunnyShader.link())
+	{
+		printf("Bunny Shader had an error: %s\n", m_bunnyShader.getLastError());
+		return false;
+	}
+#pragma endregion
+#pragma region DragonShader
+	m_dragonShader.loadShader(aie::eShaderStage::VERTEX, "./shaders/simple.vert");
+
+	//Load the fragment shader from the file
+	m_dragonShader.loadShader(aie::eShaderStage::FRAGMENT, "./shaders/simple.frag");
+	if (!m_dragonShader.link())
+	{
+		printf("Dragon Shader had an error: %s\n", m_dragonShader.getLastError());
+		return false;
+	}
+#pragma endregion
+#pragma region BuddhaShader
+	m_buddhaShader.loadShader(aie::eShaderStage::VERTEX, "./shaders/simple.vert");
+
+	//Load the fragment shader from the file
+	m_buddhaShader.loadShader(aie::eShaderStage::FRAGMENT, "./shaders/simple.frag");
+	if (!m_buddhaShader.link())
+	{
+		printf("Buddha Shader had an error: %s\n", m_buddhaShader.getLastError());
+		return false;
+	}
+#pragma endregion
+#pragma region LucyShader
+	m_lucyShader.loadShader(aie::eShaderStage::VERTEX, "./shaders/simple.vert");
+
+	//Load the fragment shader from the file
+	m_lucyShader.loadShader(aie::eShaderStage::FRAGMENT, "./shaders/simple.frag");
+	if (!m_lucyShader.link())
+	{
+		printf("Buddha Shader had an error: %s\n", m_lucyShader.getLastError());
+		return false;
+	}
+
+#pragma endregion
+#pragma endregion
+
+#pragma region Mesh Logic
+#pragma region QuadLogic
 	Mesh::Vertex vertices[4];
 	vertices[0].position = { -0.5f, 0.0f, 0.5f, 1.0f };
 	vertices[1].position = { 0.5f, 0.0f, 0.5f, 1.0f };
@@ -130,30 +207,85 @@ bool GraphicsProjectApp::LoadShaderAndMesh()
 		0,0,10,0,
 		0,0,0,1
 	};
-	m_quadTransform = glm::rotate(m_quadTransform, atan(1.14f), glm::vec3(10,0,-10));
+	m_quadTransform = glm::rotate(m_quadTransform, atan(1.14f), glm::vec3(10, 0, -10));
 #pragma endregion
-#pragma region Bunny
-	m_bunnyShader.loadShader(aie::eShaderStage::VERTEX, "./shaders/simple.vert");
+#pragma region BunnyLogic
 
-	//Load the fragment shader from the file
-	m_bunnyShader.loadShader(aie::eShaderStage::FRAGMENT, "./shaders/simple.frag");
-	if (!m_bunnyShader.link())
-	{
-		printf("Simple Shader had an error: %s\n", m_bunnyShader.getLastError());
-		return false;
-	}
 	if (m_bunnyMesh.load("./stanford/bunny.obj") == false)
 	{
 		printf("Bunny Mesh Failed!!!\n");
 		return false;
 	}
-	m_bunnyTransform =
+	m_bunnyScale =
 	{
 		0.5f, 0, 0, 0,
 		0, 0.5f, 0, 0,
 		0, 0, 0.5f, 0,
 		0, 0, 0, 1
 	};
+	m_bunnyRot = glm::mat4(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);
+	m_bunnyTranslation = glm::mat4(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 5, 0, -5, 1);
+#pragma endregion
+#pragma region DragonLogic
+
+	if (m_dragonMesh.load("./stanford/Dragon.obj") == false)
+	{
+		printf("Dragon Mesh Failed!!!\n");
+		return false;
+	}
+	m_dragonScale =
+	{
+		0.5f, 0, 0, 0,
+		0, 0.5f, 0, 0,
+		0, 0, 0.5f, 0,
+		0, 0, 0, 1
+	};
+	m_dragonRot = glm::mat4(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);
+	m_dragonTranslation = glm::mat4(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, -5, 0, -5, 1);
+#pragma endregion
+#pragma region BuddhaLogic
+	if (m_buddhaMesh.load("./stanford/Buddha.obj") == false)
+	{
+		printf("Buddha Mesh Failed!!!\n");
+		return false;
+	}
+	m_buddhaScale =
+	{
+		0.5f, 0, 0, 0,
+		0, 0.5f, 0, 0,
+		0, 0, 0.5f, 0,
+		0, 0, 0, 1
+	};
+	m_buddhaRot = glm::mat4(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);
+	m_buddhaTranslation = glm::mat4(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, -5, 0, 5, 1);
+#pragma endregion
+#pragma region LucyLogic
+
+	if (m_lucyMesh.load("./stanford/Lucy.obj") == false)
+	{
+		printf("Lucy Mesh Failed!!!\n");
+		return false;
+	}
+	m_lucyScale =
+	{
+		0.5f, 0, 0, 0,
+		0, 0.5f, 0, 0,
+		0, 0, 0.5f, 0,
+		0, 0, 0, 1
+	};
+	m_lucyRot = glm::mat4(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);
+	m_lucyTranslation = glm::mat4(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 5, 0, 5, 1);
+#pragma endregion
+#pragma endregion
+
+#pragma region Phong
+	m_phongShader.loadShader(aie::eShaderStage::VERTEX, "./shaders/phong.vert");
+	m_phongShader.loadShader(aie::eShaderStage::FRAGMENT, "./shaders/phong.frag");
+	if (m_phongShader.link() == false)
+	{
+		printf("Phong shader had an error %s\n", m_phongShader.getLastError());
+		return false;
+	}
 #pragma endregion
 	return true;
 }
@@ -171,7 +303,7 @@ void GraphicsProjectApp::DrawShadersAndMeshes(glm::mat4 a_projectionMatrix,
 	m_simpleShader.bindUniform("ProjectionViewModel", pvm);
 	m_simpleShader.bindUniform("MeshFlatColour", glm::vec4(1, 1, 1, 1));
 	
-	m_quadMesh.Draw();
+	//m_quadMesh.Draw();
 #pragma endregion
 #pragma region Bunny
 
@@ -180,7 +312,90 @@ void GraphicsProjectApp::DrawShadersAndMeshes(glm::mat4 a_projectionMatrix,
 	m_bunnyShader.bindUniform("ProjectionViewModel", pvm);
 	m_bunnyShader.bindUniform("MeshFlatColour", glm::vec4(0, 1, 0, 1));
 	//Draw bunny mesh
-	m_bunnyMesh.draw();
+	//m_bunnyMesh.draw();
 
 #pragma endregion
+#pragma region Dragon
+
+	m_dragonShader.bind();
+	pvm = m_projectionMatrix * a_viewMatrix * m_dragonTransform;
+	m_dragonShader.bindUniform("ProjectionViewModel", pvm);
+	m_dragonShader.bindUniform("MeshFlatColour", glm::vec4(0, 1, 0, 1));
+#pragma endregion 
+#pragma region Buddha
+
+	m_buddhaShader.bind();
+	pvm = m_projectionMatrix * a_viewMatrix * m_buddhaTransform;
+	m_buddhaShader.bindUniform("ProjectionViewModel", pvm);
+	m_buddhaShader.bindUniform("MeshFlatColour", glm::vec4(0, 1, 0, 1));
+#pragma endregion 
+#pragma region Lucy
+
+	m_lucyShader.bind();
+	pvm = m_projectionMatrix * a_viewMatrix * m_lucyTransform;
+	m_lucyShader.bindUniform("ProjectionViewModel", pvm);
+	m_lucyShader.bindUniform("MeshFlatColour", glm::vec4(0, 1, 0, 1));
+#pragma endregion 
+#pragma region Phong
+	//Bind the shader
+	m_phongShader.bind();
+
+	//Bind the camera position
+	m_phongShader.bindUniform("CameraPosition", vec3(glm::inverse(a_viewMatrix)[3]));
+
+	//Bind the light
+	m_phongShader.bindUniform("AmbientColour", m_ambientLight);
+	m_phongShader.bindUniform("LightColour", m_light.colour);
+	m_phongShader.bindUniform("LightDirection", m_light.direction);
+#pragma endregion
+#pragma region BunnyPhong
+	//Bind the pvm
+	pvm = a_projectionMatrix * a_viewMatrix * m_bunnyTransform;
+	m_phongShader.bindUniform("ProjectionViewModel", pvm);
+	//Bind the lighting transforms
+	m_phongShader.bindUniform("ModelMatrix", m_bunnyTransform);
+
+	m_bunnyMesh.draw();
+#pragma endregion
+#pragma region DragonPhong
+	pvm = a_projectionMatrix * a_viewMatrix * m_dragonTransform;
+	m_phongShader.bindUniform("ProjectionViewModel", pvm);
+	//Bind the lighting transforms
+	m_phongShader.bindUniform("ModelMatrix", m_dragonTransform);
+
+	m_dragonMesh.draw();
+#pragma endregion
+#pragma region BuddhaPhong
+	pvm = a_projectionMatrix * a_viewMatrix * m_buddhaTransform;
+	m_phongShader.bindUniform("ProjectionViewModel", pvm);
+	//Bind the lighting transforms
+	m_phongShader.bindUniform("ModelMatrix", m_buddhaTransform);
+
+	m_buddhaMesh.draw();
+#pragma endregion
+#pragma region LucyPhong
+	pvm = a_projectionMatrix * a_viewMatrix * m_lucyTransform;
+	m_phongShader.bindUniform("ProjectionViewModel", pvm);
+	//Bind the lighting transforms
+	m_phongShader.bindUniform("ModelMatrix", m_lucyTransform);
+
+	m_lucyMesh.draw();
+#pragma endregion
+
+}
+
+void GraphicsProjectApp::IMGUI_Logic()
+{
+	ImGui::Begin("Scene Light Settings");
+	ImGui::DragFloat3("Sunlight Direction", &m_light.direction[0], 0.1f, -1.0f, -1.0f);
+	ImGui::DragFloat3("Sunlight Colour", &m_light.colour[0], 0.1f, 0.0f, 2.0f);
+	ImGui::End();
+}
+
+void GraphicsProjectApp::IMGUI_Transform()
+{
+	ImGui::Begin("Scene Light Settings");
+	ImGui::DragFloat3("Sunlight Direction", &m_light.direction[0], 0.1f, -1.0f, -1.0f);
+	ImGui::DragFloat3("Sunlight Colour", &m_light.colour[0], 0.1f, 0.0f, 2.0f);
+	ImGui::End();
 }
--- aieBootstrap-master/aieBootstrap-master/GraphicsProject/GraphicsProjectApp.h
+++ aieBootstrap-master/aieBootstrap-master/GraphicsProject/GraphicsProjectApp.h
@@ -21,25 +21,59 @@ public:
 	void Planets();
 	bool LoadShaderAndMesh();
 	void DrawShadersAndMeshes(glm::mat4, glm::mat4);
-
+	void IMGUI_Logic();
+	void IMGUI_Transform();
 protected:
 
 	//====SHADER====
 	aie::ShaderProgram m_simpleShader;
 	aie::ShaderProgram m_bunnyShader;
+	aie::ShaderProgram m_dragonShader;
+	aie::ShaderProgram m_buddhaShader;
+	aie::ShaderProgram m_lucyShader;
+	aie::ShaderProgram m_phongShader;
 	//===================
 	//Basic Plane
 	Mesh				m_quadMesh;
-	glm::mat4			m_quadTransform;
-
+	glm::mat4			m_quadTransform = glm::mat4(0);
 	//Create a bunny with a flat colour
 	aie::OBJMesh		m_bunnyMesh;
+	glm::mat4			m_bunnyTranslation;
 	glm::mat4			m_bunnyTransform;
+	glm::mat4			m_bunnyRot;
+	glm::mat4			m_bunnyScale;
 
-	// camera transforms
+	//Creates the dragon
+	aie::OBJMesh		m_dragonMesh;
+	glm::mat4			m_dragonTransform;
+	glm::mat4			m_dragonTranslation;
+	glm::mat4			m_dragonRot;
+	glm::mat4			m_dragonScale;
+	//Creates the Buddha
+	aie::OBJMesh		m_buddhaMesh;
+	glm::mat4			m_buddhaTransform;
+	glm::mat4			m_buddhaTranslation;
+	glm::mat4			m_buddhaRot;
+	glm::mat4			m_buddhaScale;
+	//Creates a statue called lucy
+	aie::OBJMesh		m_lucyMesh;
+	glm::mat4			m_lucyTransform;
+	glm::mat4			m_lucyTranslation;
+	glm::mat4			m_lucyRot;
+	glm::mat4			m_lucyScale;
+	// Camera transforms
 	glm::mat4	m_viewMatrix;
 	glm::mat4	m_projectionMatrix;
 	// planets
 	Planet* m_planet;
 	std::vector<Planet*> m_planetsArray;
+
+	struct Light 
+	{
+		glm::vec3 direction;
+		glm::vec3 colour;
+	};
+
+	Light				m_light;
+	glm::vec3			m_ambientLight;
 };

750bacc853a798536caf4ec49360b22e4eaad42b
Benjamin McDonald <s172834@students.aie.edu.au>
2021-03-19 16:58:52 +1100

    Started working on the particles and have finished adding a new object.

--- aieBootstrap-master/aieBootstrap-master/GraphicsProject/ParticleEmmiter.cpp
+++ aieBootstrap-master/aieBootstrap-master/GraphicsProject/ParticleEmmiter.cpp
@@ -1,1 +1,16 @@
 #include "ParticleEmmiter.h"
+#include <gl_core_4_4.h>
+
+ParticleEmmiter::ParticleEmmiter():
+	m_particles(nullptr), m_firstDead(0), m_maxParticles(0),
+	m_position(0,0,0), m_vao(0), m_vbo(0), m_ibo(0), m_vertexData(nullptr)
+{
+}
+
+ParticleEmmiter::~ParticleEmmiter()
+{
+	delete[] m_particles;
+	delete[] m_vertexData;
+	glDeleteVertexArrays(1, &m_vao);
+
+}
--- aieBootstrap-master/aieBootstrap-master/GraphicsProject/ParticleEmmiter.h
+++ aieBootstrap-master/aieBootstrap-master/GraphicsProject/ParticleEmmiter.h
@@ -1,5 +1,46 @@
 #pragma once
+#include <glm/glm.hpp>
+
+struct Particle 
+{
+	glm::vec3 position;
+	glm::vec3 velocity;
+	glm::vec4 colour;
+	float size;
+	float lifeTime;
+	float lifeSpan;
+};
+struct ParticleVertex 
+{
+	glm::vec4 position;
+	glm::vec4 colour;
+};
+
 class ParticleEmmiter
 {
+public:
+	ParticleEmmiter();
+	virtual ~ParticleEmmiter();
+
+protected:
+	Particle* m_particles;
+	unsigned int m_firstDead;
+	unsigned int m_maxParticles;
+
+	unsigned int m_vao, m_vbo, m_ibo;
+	ParticleVertex* m_vertexData;
+
+	glm::vec3 m_position;
+	glm::vec4 m_startColour;
+	glm::vec4 m_endColour;
+	float m_emitTimer;
+	float m_emitRate; 
+	float m_lifespanMin;
+	float m_lifespanMax;
+	float m_velocityMin;
+	float m_velocityMax;
+	float m_startSize;
+	float m_endSize;
+
 };
 
